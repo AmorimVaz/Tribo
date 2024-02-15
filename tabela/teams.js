ddcd948eb694f151feda036734aaa5d3e9121d4c
Fabiano Vaz <amorimvaz@gmail.com>
2024-02-14 21:33:15 -0300

    Simulador -> Beta

---
+++ tabela/teams.js
@@ -0,0 +1,20 @@
+teams = [
+    {"seed": 1,"name": "Monte", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/mont.png"},
+    {"seed": 2,"name": "paiN", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/pain.png"},
+    {"seed": 3,"name": "G2", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/g2.png"},
+    {"seed": 4,"name": "Gamer Legion", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/gl.png"},
+    {"seed": 5,"name": "forZe", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/forz.png"},
+    {"seed": 6,"name": "Apeks", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/apek.png"},
+    {"seed": 7,"name": "NiP", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/nip.png"},
+    {"seed": 8,"name": "OG", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/og.png"},
+    {"seed": 9,"name": "ENCE", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/ence.png"},
+    {"seed": 10,"name": "MOUZ", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/mouz.png"},
+    {"seed": 11,"name": "Liquid", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/liqu.png"},
+    {"seed": 12,"name": "Grayhound", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/ghg.png"},
+    {"seed": 13,"name": "Complexity", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/col.png"},
+    {"seed": 14,"name": "The MongolZ", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/mong.png"},
+    {"seed": 15,"name": "Fluxo", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/flux.png"},
+    {"seed": 16,"name": "FaZe Clan", "rnd": "0-0", "score": 0, "v":0, "d":0, "match":[], "status": 0, "img":"https://majors.im/images/paris2023_rmr/faze.png"},
+]
+
+localStorage.setItem("teams", JSON.stringify(teams));
